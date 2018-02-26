039fc79942f82e8512fd08d98b1dc9462d52f8c0
pagliacci <ostrokonskiy@gmail.com>
2018-02-26 11:17:57 +0300

    added initialization script

---
+++ init_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get install python
+cd honeynode
+python get-pip.py
+pip install paramiko
+pip install pyinotify
+pip install geoip2
+pip install python-daemon
+sed -i 's/Port 22/Port 2222/g' /etc/ssh/sshd_config
+service ssh restart
